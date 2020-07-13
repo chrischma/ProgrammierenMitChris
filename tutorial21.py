ad2b6500b4c785731fc8be90a5dca36391c1c80d
chrischma <67865565+chrischma@users.noreply.github.com>
2020-07-13 14:47:44 +0200

    Add files via upload

---
+++ tutorial21.py
@@ -0,0 +1,16 @@
+# Das ist der schöne Weg, mehrzeilig auszukommentieren. 
+
+# print("A")
+# print("B")
+# print("C")
+# print("D")
+
+
+#Inoffiziell wird oftmals auch diese Schreibweise verwendet. Sollte man allerdings nicht tun, da es sich nicht um wirkliche Kommentare handelt, sondern um zweckentfremdete Strings.
+
+'''
+print("A")
+print("B")
+print("C")
+print("D")
+'''
