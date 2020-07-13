a68b3597bbeaa8267d8375e322925219967c0273
chrischma <67865565+chrischma@users.noreply.github.com>
2020-07-13 14:49:36 +0200

    Added a Tutorial about for-loops.

---
+++ tutorial_20.py
@@ -0,0 +1,11 @@
+namen = ["Chris","Gerda","Alfred","Lotte"]
+print(namen)
+
+for name in namen :
+	print(f'Hallo {name}')
+
+preise = [9.99,19.99,2500,12.99]
+
+for preis in preise:
+	preis*=0.8
+	print(f'Der neue Preis beträgt jetzt {preis}')
