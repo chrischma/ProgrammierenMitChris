59394f0dd8a9144975d7c019d07d002f2037d846
chrischma <67865565+chrischma@users.noreply.github.com>
2020-07-13 14:51:17 +0200

    Added a tutorial about arrays.

---
+++ tutorial_18.py
@@ -0,0 +1,16 @@
+import numpy
+
+#Declaring an array
+array_1 = numpy.array([1,5,7])
+
+#Calculate with an array
+array_1+200
+array_1-200
+array_1*200
+array_1/200
+
+Zusammenfassung: 
+
+viele Daten effizient nutzen oder 
+direktes rechnen
+
