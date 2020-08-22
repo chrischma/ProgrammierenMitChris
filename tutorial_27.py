3d320aeaa9597be753824dadd9733d2138a8db53
Chris <post@christoph-schmalfuss.de>
2020-08-22 15:13:32 +0200

    Added a tutorial about comparing values

---
+++ tutorial_27.py
@@ -0,0 +1,17 @@
+# Prüfen, ob Werte gleich sind
+5 == 10
+
+# Prüfen, ob Werte ungleich sind
+5 != 10
+
+# Prüfen, ob ein Wert größer ist als ein anderer Wert.
+5 > 10
+
+# Prüfen, ob ein Wert kleiner ist als ein anderer Wert.
+5 < 10
+
+# Prüfen, ob ein Wert größer oder gleich einem anderen Wert ist.
+5 >= 10
+
+# Prüfen, ob ein Wert kleiner oder gleich einem anderen Wert ist.
+5 <= 10
