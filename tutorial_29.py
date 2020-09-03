183885ddc21a1a8d0085ba0b2bffaa16a9b10a1a
Chris <post@christoph-schmalfuss.de>
2020-09-03 17:51:04 +0200

    Added a tutorial about writing custom modules

---
+++ tutorial_29.py
@@ -0,0 +1,12 @@
+# Wir legen zuvor eine Datei namens mein_modul.py an.
+
+# Diese enthält unsere Funktion:
+
+# def say_hello():
+# 	print("Hello hello!")
+
+# Wir importieren unser selbstgeschriebenes Modul
+import mein_modul
+
+# Wir führen aus "mein_modul.py" die Funktion say_hello() aus.
+mein_modul.say_hello()
