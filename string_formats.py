aeb7757839b60675a7ed8d47f9028e981f79550c
Chris <post@christoph-schmalfuss.de>
2021-04-01 19:47:27 +0200

    Update string_formats.py

--- string_formats.py
+++ string_formats.py
@@ -1,7 +1,7 @@
-farbe = "gelbe"
-name = "Alfred"
+FARBE = "gelbe"
+NAME = "Alfred"
 
-print("Das %s Auto von %s ist sehr schön." %(farbe,name))
-print("Das {} Auto von {} ist sehr schön.".format(farbe,name))
-print("Das",farbe,"Auto von",name,"ist sehr schön.")
-print(f'Das {farbe} Auto von {name} ist sehr schön.')
+print("Das %s Auto von %s ist sehr schön." %(FARBE, NAME))
+print("Das {} Auto von {} ist sehr schön.".format(FARBE, NAME))
+print("Das", FARBE, "Auto von", NAME, "ist sehr schön.")
+print(f'Das {FARBE} Auto von {NAME} ist sehr schön.')
