c5297552a9bfc2abacf4016ced57064ebba41e0a
Chris <post@christoph-schmalfuss.de>
2021-04-01 19:44:58 +0200

    cleaned up code

--- tutorial_19.py
+++ tutorial_19.py
@@ -1,13 +1,8 @@
 
-# Das (Farbe) Auto von (Name) ist schön.
-
 farbe = "gelbe"
 name = "Alfred"
 
 print("Das %s Auto von %s ist sehr schön." %(farbe,name))
-
 print("Das {} Auto von {} ist sehr schön.".format(farbe,name))
-
 print("Das",farbe,"Auto von",name,"ist sehr schön.")
-
 print(f'Das {farbe} Auto von {name} ist sehr schön.')
