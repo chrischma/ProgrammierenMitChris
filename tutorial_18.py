e6a28ac9b88408fd9e065138f979846eed2ab0cb
Chris <post@christoph-schmalfuss.de>
2021-04-01 19:35:51 +0200

    Update tutorial_18.py

--- tutorial_18.py
+++ tutorial_18.py
@@ -1,16 +1,1 @@
-import numpy
-
-#Declaring an array
-array_1 = numpy.array([1,5,7])
-
-#Calculate with an array
-array_1+200
-array_1-200
-array_1*200
-array_1/200
-
-Zusammenfassung: 
-
-viele Daten effizient nutzen oder 
-direktes rechnen
 
