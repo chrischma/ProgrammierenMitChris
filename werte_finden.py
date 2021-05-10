e97fbb1284d4fb1f59ec1436d83be3b91419c41c
Chris <post@christoph-schmalfuss.de>
2021-05-10 12:15:40 +0200

    Create werte_finden.py

---
+++ werte_finden.py
@@ -0,0 +1,9 @@
+coole_liste = [29, 87, 467, 2, 57]
+
+kleinster_wert = min(coole_liste)
+groesster_wert = max(coole_liste)
+
+pos_min = coole_liste.index(kleinster_wert)
+pos_max = coole_liste.index(groesster_wert)
+
+print(pos_min, pos_max)
