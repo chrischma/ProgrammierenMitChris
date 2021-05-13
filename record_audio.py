d5f1de6d21ca16e1a43fc455ae856853d6241b34
Chris <post@christoph-schmalfuss.de>
2021-05-13 15:51:59 +0200

    Update and rename tutorial_22.py to record_audio.py

--- record_audio.py
+++ record_audio.py
@@ -1,21 +1,13 @@
-# Wir importieren die Module, die wir brauchen.
-
 import sounddevice
 import soundfile
 import time
 
-# Wir legen die Aufnahmelänge und die Qualität fest.
-
 duration = 3
 samplerate = 44100
-
-# Wir nehmen auf. 
 
 aufnahme_1 = sounddevice.rec(duration*samplerate, samplerate=samplerate, channels=2)
 print("Aufnahme läuft.")
 time.sleep(duration)
-
-# Wir speichern eine .wav Datei.
 
 soundfile.write("Aufnahme_1.wav",aufnahme_1, samplerate)
 print("Okay, ich bin fertig! :) ")
