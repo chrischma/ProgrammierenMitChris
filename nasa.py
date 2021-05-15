ad6917e9cf035b5b1cebabf03f2151b5421217c7
Chris <post@christoph-schmalfuss.de>
2021-05-15 13:05:58 +0200

    Added a tutorial about the NASA API

---
+++ nasa.py
@@ -0,0 +1,15 @@
+import requests
+
+limit = 500
+days = 356
+
+url = f'https://eonet.sci.gsfc.nasa.gov/api/v2.1/events?limit={limit}&days={days}'
+r = requests.get(url)
+events_data = r.json()
+event_list = events_data['events']
+
+for event in event_list:
+		# print(event)
+    if 'fire' in str(event['categories']):
+        print(event['title'])
+      
